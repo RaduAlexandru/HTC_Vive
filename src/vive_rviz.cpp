4ce7add68ad5079ba11abb3fce5df886d0d29225
RaduAlexandru <alejandro.rosu@gmail.com>
2016-12-12 14:10:18 +0100

    It shows a texture now. Altought in vr it looks slightly different. Must chech with an RGB image

--- src/vive_rviz.cpp
+++ src/vive_rviz.cpp
@@ -42,12 +42,11 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #define checkImageWidth 64
 #define checkImageHeight 64
 
+#include <map>
+
 class Animation:public Vrui::Application,public GLObject
 	{
 	/* Embedded classes: */
-
-	public:
-	private:
 
 
 	struct Vertex
@@ -85,6 +84,15 @@ class Animation:public Vrui::Application,public GLObject
 	
 	void* read_data(void);
 	void makeCheckImage(void);
+	void saveGLState(void);
+	void restoreGLState();
+	void SetGLCapability(GLenum capability, GLboolean state);
+
+	GLboolean SavedLighting;
+    	GLboolean SavedDepthTest; 
+    	GLboolean SavedBlending;
+	std::map<std::string, int> GLStateIntegers;	
+
 	
 	/* Constructors and destructors: */
 	public:
@@ -140,7 +148,7 @@ void* Animation::read_data(void){
 
 }
 
-/*
+
 void Animation::makeCheckImage(void)
 {
    int i, j, c;
@@ -154,22 +162,27 @@ void Animation::makeCheckImage(void)
          checkImage[i][j][3] = (GLubyte) 255;
       }
    }
-}*/
+}
 
 Animation::Animation(int& argc,char**& argv)
 	:Vrui::Application(argc,argv)
 	{
 
 
-	//makeCheckImage();
+	makeCheckImage();
 
 	
-	Vertex v=  { -0.5, -0.5, 0.0, 0.0, 0.0};
-	vertices.push_back(v);
-	Vertex v2=  {0.5, 0.0, 0.0, 1.0, 0.0};
+	Vertex v2=  {5, 0.0, 0.0, 1.0, 0.0};
 	vertices.push_back(v2);
-	Vertex v3=  {0.0, 0.5, 0.0, 0.0, 1.0};
+	Vertex v=  { -5, -5, 0.0, 0.0, 0.0};
+	vertices.push_back(v);
+	Vertex v3=  {0.0, 5, 0.0, 0.0, 1.0};
 	vertices.push_back(v3);
+
+
+	indices.push_back(0);
+	indices.push_back(1);
+	indices.push_back(2);
 	
 	read_thread.start(this,&Animation::read_data);
 	}
@@ -196,7 +209,7 @@ void Animation::display(GLContextData& contextData) const
 	{
 
 	
-	/*glPushMatrix();
+/*	glPushMatrix();
 	
 	glTranslated(-5.0,0.0,0.0);
 	glMaterialAmbientAndDiffuse(GLMaterialEnums::FRONT,GLColor<GLfloat,4>(1.0f,0.5f,0.5f));
@@ -206,22 +219,172 @@ void Animation::display(GLContextData& contextData) const
 	glMaterialAmbientAndDiffuse(GLMaterialEnums::FRONT,GLColor<GLfloat,4>(0.5f,0.5f,1.0f));
 	glDrawSphereIcosahedron(4.5f,6);
 	
-	glPopMatrix();*/
-
+	glPopMatrix();
+*/
 	DataItem* dataItem=contextData.retrieveDataItem<DataItem>(this);
 
 	// Black background
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// Draw VBO
-	glPushMatrix();
+
+
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+ 	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+
+	glBindBufferARB(GL_ARRAY_BUFFER_ARB,dataItem->VBO_id[0]);
+ 	glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER_ARB,dataItem->indexBufferId[0]);
+  	glBindTexture(GL_TEXTURE_2D, dataItem->texture[0]);  //TODO maybe  I also need to bund the texture
+
+  	glVertexPointer(3, GL_FLOAT, sizeof(GLfloat)*5, NULL);
+  	glTexCoordPointer(2, GL_FLOAT, sizeof(GLfloat)*5, (float*)(sizeof(GLfloat)*3)); //TODO this may need to change to a 2
+
+
+
+
+	 glEnable(GL_TEXTURE_2D);                        // Enable Texture Mapping ( NEW )
+    	glShadeModel(GL_SMOOTH);                        // Enable Smooth Shading
+    	//glClearColor(0.0f, 0.0f, 0.0f, 0.5f);                   // Black Background
+    	//glClearDepth(1.0f);                         // Depth Buffer Setup
+    	//glEnable(GL_DEPTH_TEST);                        // Enables Depth Testing
+    	//glDepthFunc(GL_LEQUAL);     
+
+
+
+
+
+	//save stuff
+	const_cast<Animation*>( this )->        saveGLState();
+        glPushAttrib( GL_ALL_ATTRIB_BITS );
+        glPushClientAttrib( GL_CLIENT_ALL_ATTRIB_BITS );
+        glMatrixMode( GL_COLOR );
+        glPushMatrix();
+        glMatrixMode( GL_TEXTURE );
+        glPushMatrix();
+        glMatrixMode( GL_PROJECTION );
+        glPushMatrix();
+        glMatrixMode( GL_MODELVIEW );
+        glPushMatrix();
+
+	//glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, indexPtr);
+	//glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, NULL);
+	//restore stuff
+
+	glMatrixMode( GL_COLOR );
+        glPopMatrix();
+        glMatrixMode( GL_TEXTURE );
+        glPopMatrix();
+        glMatrixMode( GL_PROJECTION );
+        glPopMatrix();
+        glMatrixMode( GL_MODELVIEW );
+        glPopMatrix();
+        glPopAttrib();
+        glPopClientAttrib();
+        const_cast<Animation*>( this )->        restoreGLState();
+
+
+	glBindBufferARB(GL_ARRAY_BUFFER_ARB,0);
+	glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER_ARB,0);
+  	glBindTexture(GL_TEXTURE_2D,0);
+
+  	glDisableClientState(GL_VERTEX_ARRAY);
+  	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+
+
+
+
+	/*// Draw VBO
+	const_cast<Animation*>( this )->	saveGLState();
+
+
+
+	glPushAttrib( GL_ALL_ATTRIB_BITS );
+        glPushClientAttrib( GL_CLIENT_ALL_ATTRIB_BITS );
+        glMatrixMode( GL_COLOR );
+        glPushMatrix();
+        glMatrixMode( GL_TEXTURE );
+        glPushMatrix();
+        glMatrixMode( GL_PROJECTION );
+        glPushMatrix();
+        glMatrixMode( GL_MODELVIEW );
+        glPushMatrix();
+
+	//glPushMatrix();
 	glBindBufferARB(GL_ARRAY_BUFFER, dataItem->VBO_id[0]);
 	glVertexPointer(3, GL_FLOAT, sizeof(Vertex), 0);
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glPopMatrix();
+	//glPopMatrix();
 
 
+	
+        glMatrixMode( GL_COLOR );
+        glPopMatrix();
+        glMatrixMode( GL_TEXTURE );
+        glPopMatrix();
+        glMatrixMode( GL_PROJECTION );
+        glPopMatrix();
+        glMatrixMode( GL_MODELVIEW );
+        glPopMatrix();
+        glPopAttrib();
+        glPopClientAttrib();
+
+
+	const_cast<Animation*>( this )->	restoreGLState();*/
+
+}
+
+
+void Animation::saveGLState(void){
+
+   glGetIntegerv(GL_ACTIVE_TEXTURE, &this->GLStateIntegers["GL_ACTIVE_TEXTURE"]);
+
+
+
+
+	int max;
+	GLint value;
+    glGetIntegerv(static_cast<GLenum>(0x8B4D),&value);
+    max=static_cast<int>(value);
+
+
+//	std::cout << "max units is" << max<< std::endl;
+if (this->GLStateIntegers["GL_ACTIVE_TEXTURE"] < 0 || this->GLStateIntegers["GL_ACTIVE_TEXTURE"] > max ){
+
+	this->GLStateIntegers["GL_ACTIVE_TEXTURE"] = 0;
+}	
+	
+
+    this->SavedLighting = glIsEnabled(GL_LIGHTING);
+    this->SavedDepthTest = glIsEnabled(GL_DEPTH_TEST);
+    this->SavedBlending = glIsEnabled(GL_BLEND);
+
+
+}
+
+void Animation::restoreGLState(void){
+
+	SetGLCapability(GL_LIGHTING, SavedLighting);
+        SetGLCapability(GL_DEPTH_TEST, SavedDepthTest);
+        SetGLCapability(GL_BLEND, SavedBlending);
+
+
+	glActiveTexture(GL_TEXTURE0 + this->GLStateIntegers["GL_ACTIVE_TEXTURE"]);
+
+}
+
+
+void Animation::SetGLCapability(GLenum capability, GLboolean state){
+    if (state)
+    {
+      glEnable(capability);
+    }
+    else
+    {
+      glDisable(capability);
+    }
 }
 
 void Animation::resetNavigation(void)
@@ -240,18 +403,6 @@ void Animation::initContext(GLContextData& contextData) const
 	contextData.addDataItem(this,dataItem);
 
 
-	/*Vertex vertices[] = {
-	{-0.5, -0.5, 0.0, 0.0, 0.0},
-	{0.5, 0.0, 0.0, 0.0, 0.0},
-	{0.0, 0.5, 0.0, 0.0, 0.0},
-	};*/
-	
-
-
-
-//	glBindBufferARB(GL_ARRAY_BUFFER,dataItem-> VBO_id[0]);
-//	glBufferDataARB(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);	
-
 
 
 	/* Upload all vertices into the vertex buffer: */
@@ -266,6 +417,21 @@ void Animation::initContext(GLContextData& contextData) const
 
 
 
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glBindTexture(GL_TEXTURE_2D, dataItem->texture[0]);
+
+   	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+   	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+   	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+   	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+   	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth, 
+                checkImageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, 
+                checkImage);
 	}
+
+
+
+	//	GLuint shaderProgram = create_program("shaders/vert.shader", "shaders/frag.shader");
 
 VRUI_APPLICATION_RUN(Animation)
